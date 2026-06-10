581ab215c0160bc5b929c19fce8b78922f5a0a64
Kənan Gərayzadə <kenangerayzade2007@gmail.com>
2026-06-10 18:08:00 +0000

    Add task 5

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -silent -d $1 | tee >(while read s; do echo "$s,$(host $s | awk '/has address/ {print $4; exit}')"; done > $1.txt)
+subfinder -silent -d $1 | tee >(while read sub; do echo $sub,$(dig +short $sub | grep -E "^[0-9.]+$" | head -1); done > $1.txt)
