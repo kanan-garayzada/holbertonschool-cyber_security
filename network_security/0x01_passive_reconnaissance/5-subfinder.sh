3db0bb0bebfe948b14beaf0245f5d22de9e64a52
kanan-garayzada <kenangerayzade2007@gmail.com>
2026-06-19 10:28:23 +0000

    Rewriting the code

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-
-subfinder -silent -d "$1" | tee >(while read -r sub; do
-    host "$sub" | awk -v sub="$sub" '/has address/ {print sub "," $4}'
-done > "$1.txt")
+subfinder -silent -d $1 -o $1.txt -nW -oI
