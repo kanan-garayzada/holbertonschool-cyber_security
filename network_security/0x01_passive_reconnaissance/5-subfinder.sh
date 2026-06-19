aa6a4bd644eb9e9e6aba0daa1a77d6456ad80513
kanan-garayzada <kenangerayzade2007@gmail.com>
2026-06-19 10:23:09 +0000

    Fix

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-subfinder -silent -d $1 | tee >(while read sub; do host $sub | awk "/has address/ {print \"$sub,\" \$4}"; done > $1.txt)
+
+subfinder -silent -d "$1" | tee >(while read -r sub; do
+    host "$sub" | awk -v sub="$sub" '/has address/ {print sub "," $4}'
+done > "$1.txt")
